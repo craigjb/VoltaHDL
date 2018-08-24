a2c1805a87ff2ebd9caf49a1a3aba1b58c9ef10d
Craig Bishop <craig@craigjb.com>
2018-08-24 16:06:12 -0700

    Add decoupling helper for STM32L0 parts.

--- voltalib/stm32/l0/__init__.py
+++ voltalib/stm32/l0/__init__.py
@@ -1,11 +1,27 @@
 import os.path
 
+from voltahdl import C
+
 from ..base import STM32Component, generate_stm32_component
 
 
 class STM32L0Component(STM32Component):
     def __init__(self):
         super().__init__()
+
+    def decouple_vdd(self):
+        """
+        Generates decoupling capacitors for the VDD rail per ST recommendations
+
+        Follows page 11 at:
+        https://www.st.com/content/ccc/resource/technical/document/application_note/fd/f4/6b/40/9a/b5/41/a2/DM00112257.pdf/files/DM00112257.pdf/jcr:content/translations/en.DM00112257.pdf
+        """
+        self.pins.VDD > C('10 uF') < self.pins.VSS
+        if isinstance(self.pins.VDD.number, list):
+            for _ in range(len(self.pins.VDD.number)):
+                self.pins.VDD > C('100 nF') < self.pins.VSS
+        else:
+            self.pins.VDD > C('100 nF') < self.pins.VSS
 
 
 STM32L073RZ = generate_stm32_component(
